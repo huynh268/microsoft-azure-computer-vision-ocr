441bc9b9eaec7c3e41d505223effc10ef4f4367a
huynh268 <TienHuynh1@Tiens-MacBook-Air.local>
2017-06-26 16:15:32 -0500

    Add OCR using Microsoft Azure Computer Vision

---
+++ ocr.py
@@ -0,0 +1,112 @@
+########### Python 2.7 #############
+import httplib, urllib, base64, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = '45c48107d2064adcbae152c77a0d1e5f'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.urlencode({
+    # Request parameters. The language setting "unk" means automatically detect the language.
+    'language': 'unk',
+    'detectOrientation ': 'true',
+})
+
+# The URL of a JPEG image containing text.
+body = "{'url':'https://www.prepressure.com/images/fonts_sample_ocrb_medium.png'}"
+
+try:
+    # Execute the REST API call and get the response.
+    conn = httplib.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    #print (json.dumps(parsed, sort_keys=True, indent=2))
+    for line in parsed['regions'][0]['lines']:
+        for word in line['words']:
+            print(json.dumps(word['text'], sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print('Error:')
+    print(e)
+
+####################################
+
+'''
+########### Python 3.6 #############
+import http.client, urllib.request, urllib.parse, urllib.error, base64, json
+
+###############################################
+#### Update or verify the following values. ###
+###############################################
+
+# Replace the subscription_key string value with your valid subscription key.
+subscription_key = '45c48107d2064adcbae152c77a0d1e5f'
+
+# Replace or verify the region.
+#
+# You must use the same region in your REST API call as you used to obtain your subscription keys.
+# For example, if you obtained your subscription keys from the westus region, replace 
+# "westcentralus" in the URI below with "westus".
+#
+# NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+# a free trial subscription key, you should not need to change this region.
+uri_base = 'westcentralus.api.cognitive.microsoft.com'
+
+headers = {
+    # Request headers.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': subscription_key,
+}
+
+params = urllib.parse.urlencode({
+    # Request parameters. The language setting "unk" means automatically detect the language.
+    'language': 'unk',
+    'detectOrientation ': 'true',
+})
+
+# The URL of a JPEG image containing text.
+body = "{'url':'https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Atomist_quote_from_Democritus.png/338px-Atomist_quote_from_Democritus.png'}"
+
+try:
+    # Execute the REST API call and get the response.
+    conn = http.client.HTTPSConnection('westcentralus.api.cognitive.microsoft.com')
+    conn.request("POST", "/vision/v1.0/ocr?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+
+    # 'data' contains the JSON data. The following formats the JSON data for display.
+    parsed = json.loads(data)
+    print ("Response:")
+    print (json.dumps(parsed, sort_keys=True, indent=2))
+    conn.close()
+
+except Exception as e:
+    print('Error:')
+    print(e)
+
+####################################
+'''
